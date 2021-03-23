f4a3f85bc001e8c70ddcf56397add4dacadbf1c9
John P Grogan <john.grogan@ndcn.ox.ac.uk>
2021-03-23 18:05:13 +0000

    option to ignore fields too. deal with data.respPdf (cell), and try/catch for other fields

--- MemUtilities2D/ChangeMTB2DUnits.m
+++ MemUtilities2D/ChangeMTB2DUnits.m
@@ -1,4 +1,4 @@
-function data = ChangeMTB2DUnits(data, denominator, fieldsToConvert)
+function data = ChangeMTB2DUnits(data, denominator, fieldsToConvert, fieldsToIgnore)
 % change all coord units in a MemToolbox2D data structure
 % Can use data.dimensions to normalise to 0-1 (or data.dimensions/100 for
 % 0-100), or can use pixelsPerMm to convert into mm (and thereafter into 
@@ -9,8 +9,9 @@ function data = ChangeMTB2DUnits(data, denominator, fieldsToConvert)
 %   denominator = factor to divide by to change units. either [x; y] or
 %     scalar. (to make proportion of dimensions use data.dimensions, to
 %     make percentage of dimensions, use data.dimesions/100).
-%   fieldsToConvert = cell array of fieldnames to convert. default is all
-%       but data.n and data.whichIsTestItem
+%   fieldsToConvert = cell array of fieldnames to convert. default is all.
+%   fieldsToIgnore = cell array of fieldsnames to not convert. default is
+%       data.n and data.whichIsTestItem
 % Outputs:
 %   data = data structure in new units
 % 
@@ -30,31 +31,46 @@ if ~ismatrix(denominator) || numel(denominator) >2
 elseif all(size(denominator) == [1 2])
     denominator = denominator'; % rotate into column
 end
-if exist('fieldsToConvert','var')
+if exist('fieldsToConvert','var') && ~isempty(fieldsToConvert)
     if ~iscell(fieldsToConvert)
         error('fieldsToConvert must be a cell array of strings')
     end
 else
-    fieldsToConvert = fieldNames(~ismember( fieldNames,{'whichIsTestItem','n'})); % all but n and whichIsTestItem
+    fieldsToConvert = fieldNames; % all
+end
+if exist('fieldsToIgnore','var') && ~isempty(fieldsToIgnore)
+    if ~iscell(fieldsToIgnore)
+        error('fieldsToIgnore must be a cell array of strings')
+    end
+else
+    fieldsToIgnore = {'whichIsTestItem','n'}; % all but n and whichIsTestItem
 end
 
 
 %% use common names between fields and fieldsToConvert
 
-fieldNames = fieldNames(ismember(fieldNames, fieldsToConvert));
+fieldNames = fieldNames(ismember(fieldNames, fieldsToConvert)); 
+
+% remove fieldsToIgnore
+fieldNames = fieldNames(~ismember(fieldNames, fieldsToIgnore));
+
 
 %% convert them
 for i = 1:length(fieldNames)
-    x = data.(fieldNames{i}); % copy daty
-    
-    r = size(x,1) ./ size(denominator,1);
-    if mod(r, 1) == 0 % if is has even rows
-        denom1 = repmat(denominator, r, 1); % repeat rows to match size       
+    x = data.(fieldNames{i}); % copy data
+    try
+        if iscell(x) % resppdf
+            data.(fieldNames{i}) = cellfun(@(x) x ./ denom1, x, 'UniformOutput',0);
+        else
+            r = size(x,1) ./ size(denominator,1);
+            if mod(r, 1) == 0 % if is has even rows
+                denom1 = repmat(denominator, r, 1); % repeat rows to match size       
+                data.(fieldNames{i}) = x ./ denom1;
+            end
+        end
         
-        data.(fieldNames{i}) = x ./ denom1;
-    
-    else
-        error('number of rows in field does not allow division by denominator');
+    catch ME
+        warning('ChangeMTB2DUnits:TypeError', '%s, %s', ME.identifier, ME.message);
     end
     
     
